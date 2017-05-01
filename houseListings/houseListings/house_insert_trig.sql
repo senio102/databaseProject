c886e7ebc553fdddd9fa9991cc8e4c9bed692141
senio102 <senio102@VDI-CPSC-06>
2017-05-01 09:01:01 -0700

    query

---
+++ houseListings/houseListings/house_insert_trig.sql
@@ -0,0 +1,39 @@
+﻿create trigger house_all_insert_trig
+	on House_All
+	instead of insert
+	as
+	begin
+		declare @house_price int(9)
+		declare @bedroom_count int(2)
+		declare @bathroom_count int(2)
+		declare @zip_code int(5)
+		declare @city varchar(40)
+		declare @state varchar(2)
+		declare @sqft int(5)
+		declare @house_year int(4)
+		select * into #ttable from inserted
+		begin
+			while(exists(select account_email from #ttable))
+				begin
+					select top 1 @house_price = house_price from #ttable
+					select top 1 @bedroom_count = bedroom_count from #ttable
+					select top 1 @bathroom_count = bathroom_count from #ttable
+					select top 1 @zip_code = zip_code from #ttable
+					select top 1 @city = city from #ttable
+					select top 1 @state = state from #ttable
+					select top 1 @sqft = sqft from #ttable
+					select top 1 @house_year = house_year from #ttable
+
+					insert into House_Price values(@house_price)
+					insert into Bedroom_Count values (@bedroom_count)
+					insert into Bathroom_Count values (@bathroom_count)
+					insert into zip_code values (@zip_code)
+					insert into city values (@city)
+					insert into us_state values (@state)
+					insert into house_sqft values (@sqft)
+					insert into year_built values (@house_year)
+
+					delete from #ttable where account_email = @account_email
+				end
+		end
+	end
---
+++ houseListings/house_create_query.sql
@@ -0,0 +1,60 @@
+﻿create table House_Identifier(
+	house_id int IDENTITY(1,1),
+	primary key(house_id)
+)
+
+create table House_Price(
+	house int(10),
+	price int(9),
+	foreign key(house) references House_Identifier(house_id),
+	primary key(house)
+)
+
+create table Bedroom_Count(
+	house int(10),
+	bedroom int(2),
+	foreign key(house) references House_Identifier(house_id),
+	primary key(house)
+)
+
+create table Bathroom_Count(
+	house int(10),
+	bathroom int(2),
+	foreign key(house) references House_Identifier(house_id),
+	primary key(house)
+)
+
+create table zip_code(
+	house int(10),
+	zip int(5),
+	foreign key(house) references House_Identifier(house_id),
+	primary key(house)
+)
+
+create table city(
+	house int(10),
+	city varchar(40),
+	foreign key(house) references House_Identifier(house_id),
+	primary key(house)
+)
+
+create table us_state(
+	house int(10),
+	state varchar(2),
+	foreign key(house) references House_Identifier(house_id),
+	primary key(house)
+)
+
+create table house_sqft(
+	house int(10),
+	sqft int(5),
+	foreign key(house) references House_Identifier(house_id),
+	primary key(house)
+)
+
+create table year_built(
+	house int(10),
+	house_year int(4),
+	foreign key(house) references House_Identifier(house_id),
+	primary key(house)
+)
